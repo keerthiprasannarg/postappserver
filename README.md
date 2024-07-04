d8e3e219bce2b076c3943bf553b3f3d70413e259
keerthiprasannarg <109460040+keerthiprasannarg@users.noreply.github.com>
2024-07-04 13:00:18 -0400

    started uploading the files

---
+++ README.md
@@ -0,0 +1,7 @@
+### This is a MERN stack application
+
+## Below is the ER Diagram
+
+
+
+<img width="822" alt="Screenshot 2024-06-03 at 8 40 48 AM" src="https://github.com/keerthiprasannarg/postapplication/assets/109460040/56f213be-2c9d-4ef5-8fd9-2b0464628b89">
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const { connect } = require("./server/database");
+app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-Width,Content-Type,Accept,Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+app.use(express.static(__dirname + "/public"));
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public", "index.html"))
+// );
+
+app.use("/", require("./server/routes/index"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}!`);
+  connect();
+});
